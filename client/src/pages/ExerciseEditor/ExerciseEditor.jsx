87d27427b034ef053c71a0f75800427a008d1ddf
VoiDd2567 <rozhanskiyivan@gmail.com>
2025-12-14 23:18:09 +0200

    Autocheck session on ExerciseEditor

--- client/src/pages/ExerciseEditor/ExerciseEditor.jsx
+++ client/src/pages/ExerciseEditor/ExerciseEditor.jsx
@@ -11,6 +11,7 @@ const ExerciseEditor = () => {
     const { t } = useTranslation();
     const [autocheck, setAutocheck] = useState(true)
     const [autocheckType, setAutocheckType] = useState(t("output_check_input"))
+    const [autocheckCheckAmount, setAutocheckCheckAmount] = useState(1)
 
     return <div className="exercise_editor_page">
         <MinimizedHeader />
@@ -20,9 +21,8 @@ const ExerciseEditor = () => {
                 <textarea className="exercise_editor_page-form-item-textarea" autoFocus></textarea>
             </div>
             <DescriprionBlock />
-            <div className="exercise_editor_page-form-section_name">{t("autocheck")}</div>
             <div className="exercise_editor_page-form-select">
-                <div className="exercise_editor_page-form-item-label small-label">{t("autocheck")}</div>
+                <div className="exercise_editor_page-form-section_name">{t("autocheck")}</div>
                 <input type="checkbox" className="exercise_editor_page-form-item-checkbox" onChange={() => setAutocheck(!autocheck)} checked={autocheck} />
             </div>
             {autocheck && (
@@ -41,10 +41,21 @@ const ExerciseEditor = () => {
                                 <div className="exercise_editor_page-form-item-label">{t("function_name")}</div>
                                 <textarea className="exercise_editor_page-form-item-textarea small-textarea"></textarea>
                             </div>)}
+                        {(autocheckType === t("func_check") || autocheckType === t("output_check_input")) && (
+                            <div className="exercise_editor_page-form-item">
+                                <div className="exercise_editor_page-form-item-label">{t("check_amount")}</div>
+                                <input type="number" min="1" className="exercise_editor_page-form-item-counter" value={autocheckCheckAmount} onChange={(e) => setAutocheckCheckAmount(e.target.value)}></input>
+                            </div>)}
                     </div>
-                    {(autocheckType === t("output_check_input") || autocheckType === t("func_check")) && (<AutocheckValues />)}
+                    {autocheckType === t("output_check") && (
+                        <div className="exercise_editor_page-form-item">
+                            <div className="exercise_editor_page-form-item-label">{t("Output")}</div>
+                            <textarea className="exercise_editor_page-form-item-textarea"></textarea>
+                        </div>)}
+                    {(autocheckType === t("output_check_input") || autocheckType === t("func_check")) && (<AutocheckValues inputAmount={autocheckCheckAmount} />)}
                 </>
             )}
+            <div className="exercise_editor_page_bottom"></div>
         </div>
     </div>
 }
--- client/src/pages/ExerciseEditor/componenets/autocheckValues.jsx
+++ client/src/pages/ExerciseEditor/componenets/autocheckValues.jsx
@@ -1,6 +1,111 @@
-const AutocheckValues = () => {
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import arrow_r from "../../../pictures/arrow-r.png"
+import deleteImg from "../../../pictures/delete.png";
+import deleteRedImg from "../../../pictures/delete-red.png";
+
+const AutocheckValues = ({ inputAmount = 2 }) => {
+    const { t } = useTranslation()
+
+    const [paires, setPairs] = useState([{ input: [], output: "" }])
+
+    useEffect(() => {
+        setPairs(prev =>
+            prev.map(pair => ({
+                ...pair,
+                input: normalizeInput(pair.input)
+            }))
+        );
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [inputAmount])
+
+    useEffect(() => {
+        if (paires.length <= 0) {
+            const inputArr = Array(inputAmount).fill("")
+            setPairs([{ input: inputArr, output: "" }])
+        }
+    }, [paires, inputAmount])
+
+    const normalizeInput = (arr) => [
+        ...arr.slice(0, inputAmount),
+        ...Array(Math.max(0, inputAmount - arr.length)).fill("")
+    ];
+
+    const handleAddClick = () => {
+        setPairs(prev => [
+            ...prev,
+            { input: Array(inputAmount).fill(""), output: "" }
+        ]);
+    };
+
+    const deleteItem = (idx) => {
+        setPairs(prev => prev.filter((_, index) => index !== idx));
+    };
+
+    const updatePairInput = (pairIdx, inputIdx, value) => {
+        setPairs(prev =>
+            prev.map((pair, index) =>
+                index === pairIdx
+                    ? {
+                        ...pair,
+                        input: pair.input.map((inp, idx) =>
+                            idx === inputIdx ? value : inp
+                        )
+                    }
+                    : pair
+            )
+        );
+    };
+
+    const updatePairOutput = (pairIdx, value) => {
+        setPairs(prev =>
+            prev.map((pair, index) =>
+                index === pairIdx
+                    ? { ...pair, output: value }
+                    : pair
+            )
+        );
+    };
+
     return (
-        <div></div>
+        <div>
+            <div className="exercise_editor_page-form-item">
+                <button className="autocheck_values-menu-add_btn" onClick={handleAddClick}>{t('add_pair')}</button>
+                <div className="autocheck_values-menu-pairs">
+                    {paires.map(({ input, output }, index) => (
+                        <div key={index} className="autocheck_values-menu-pair-wrap">
+                            <div className="autocheck_values-menu-pair">
+                                <div className="autocheck_values-menu-pair-item">
+                                    {input.map((inputOne, idx) => (
+                                        <div key={idx} className="autocheck_values-menu-pair-item-input">
+                                            <div className="autocheck_values-menu-pair-label">{t("Input")} {idx + 1}</div>
+                                            <textarea
+                                                className="autocheck_values-menu-pair-textarea"
+                                                onChange={e => updatePairInput(index, idx, e.target.value)}
+                                                value={inputOne}
+                                            ></textarea>
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className="autocheck_values-menu-pair-arrow"><img src={arrow_r} alt="Arrow" /></div>
+                                <div className="autocheck_values-menu-pair-item">
+                                    <div className="autocheck_values-menu-pair-label">{t("output")}</div>
+                                    <textarea
+                                        className="autocheck_values-menu-pair-textarea"
+                                        onChange={e => updatePairOutput(index, e.target.value)}
+                                        value={output}
+                                    ></textarea>
+                                </div>
+                                <div className="autocheck_values-menu-pair-del" onClick={() => deleteItem(index)} style={{ backgroundImage: `url(${deleteImg})` }}
+                                    onMouseEnter={e => e.currentTarget.style.backgroundImage = `url(${deleteRedImg})`}
+                                    onMouseLeave={e => e.currentTarget.style.backgroundImage = `url(${deleteImg})`}></div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 
