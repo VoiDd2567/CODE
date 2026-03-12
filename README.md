51bb01cfee280ea036f3b1ad82b1fe8ffcc51245
VoiDd2567 <rozhanskiyivan@gmail.com>
2026-03-12 22:24:51 +0200

    README + new server file

---
+++ README.md
@@ -0,0 +1,19 @@
+The project can be found at the URL:
+        <https://codeest.online/>
+
+If your browser says that the connection is not secure and doesn't allow you to access the webpage, then:
+    --Press "Advanced"
+    --Press "Proceed to codeest.online (unsafe)"
+
+This is a platform called CODE (very creative, I know).
+
+On this platform you have two paths:
+    -be a teacher and create exercises
+    -be a student and solve exercises or just run your code
+
+You can create and solve Python exercises and run Python code. Node.js code can also be written and run, but you can't create exercises for Node.js.
+
+To register, you need an email.
+
+If you have any questions about the project or anything else:
+        <ivan.rozhanskiy@opilane.tammegymnaasium.ee>
--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cors = require('cors');
-const https = require('https');
-const fs = require('fs');
 const cookieParser = require('cookie-parser');
 const rateLimit = require("express-rate-limit");
 const expressSession = require("express-session");
@@ -10,39 +8,34 @@ const mongoConnect = require("./database/mongo_connect");
 const { ensureRedis } = require('./middlewares/redisStart');
 
 const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000,
-    max: 60,
-    standardHeaders: true,
-    legacyHeaders: false,
-    message: 'Too many requests, wait some time.'
+  windowMs: 1 * 60 * 1000,
+  max: 60,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: 'Too many requests, wait some time.'
 })
 
 const app = express();
-//app.use(cors({ origin: 'https://80.77.25.86:3000', credentials: true }));
-app.use(cors({ origin: 'https://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://codeest.online', credentials: true }));
+//app.use(cors({ origin: 'https://localhost:3000', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(limiter);
 //app.set('trust proxy', true); Set on for cloudflare
 
 (async () => {
-    await ensureRedis();
-    await mongoConnect();
+  await ensureRedis();
+  await mongoConnect();
 
-    app.use(require("./middlewares/session"));
+  app.use(require("./middlewares/session"));
 
-    app.use("/api/user", require("./routes/user"));
-    app.use("/api/auth", require("./routes/auth"));
-    app.use("/api/exercise", require("./routes/exercise"));
-    app.use("/api/code", require("./routes/code"));
+  app.use("/api/user", require("./routes/user"));
+  app.use("/api/auth", require("./routes/auth"));
+  app.use("/api/exercise", require("./routes/exercise"));
+  app.use("/api/code", require("./routes/code"));
 
-    const PORT = 3001;
-    const options = {
-        key: fs.readFileSync('../server.key'),
-        cert: fs.readFileSync('../server.cert')
-    };
-
-    https.createServer(options, app).listen(PORT, () => {
-        console.log(`HTTPS server working on ${PORT}`);
-    });
+  const PORT = 3001;
+  app.listen(PORT, '127.0.0.1', () => {
+    console.log(`HTTP server listening on 127.0.0.1:${PORT}`);
+  });
 })();
